aebf6496007d7b114a417ec932d21a53f10e19ef
raul1424 <raul.juarez@estudiante.uacm.edu.mx>
2021-12-05 03:42:31 -0600

    indiceParam

--- principal/urls.py
+++ principal/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("",views.holaDjango, name="holaDjango"),
     path("pepe",views.pepe, name="Hola pepe!"),
+    path('indice', views.indice, name='indice'),
+     path('indice/<str:nombre>', views.indiceParam, name='indiceParam'),
     path('<str:nombre>', views.holaTu, name='Hola Tu!')
 ]
--- principal/views.py
+++ principal/views.py
@@ -8,4 +8,10 @@ def pepe(request):
     return HttpResponse("<h1>Hola pepe!</h1>")
 
 def holaTu(request, nombre):
-    return HttpResponse(f'Hola {nombre.capitalize()}!')
+    return HttpResponse(f'<h1>Hola {nombre.capitalize()}!</h1>')
+
+def indice(request):
+    return render(request,"principal/index.html")
+
+def indiceParam(request, nombre):
+    return render(request, 'principal/saludo.html', {'nombre':nombre.capitalize()})
