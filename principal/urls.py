b57ecd1df341737686bb1432ecd694f936f13046
raul1424 <raul.juarez@estudiante.uacm.edu.mx>
2021-12-05 02:16:58 -0600

    hola pepe

--- principal/urls.py
+++ principal/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.holaDjango, name="holaDjango"),
+    path("pepe",views.pepe, name="Hola pepe!"),
 ]
--- principal/views.py
+++ principal/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def holaDjango(request):
     return HttpResponse("<h1>Hola Django!</h1>")
+
+def pepe(request):
+    return HttpResponse("<h1>Hola pepe!</h1>")
