3924a761506fd9528d6d7699d6102725e773bc93
raul1424 <raul.juarez@estudiante.uacm.edu.mx>
2021-12-05 05:52:23 -0600

    prueba viernes

---
+++ viernes/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from datetime import datetime
+
+def indice(request):
+    return render(request,"viernes/index.html" , {
+        'esViernes':datetime.today().isoweekday()==2
+    })
